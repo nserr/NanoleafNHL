044232481c73f206a3f8f58c67434f1a1df102ef
noahserr <noah.serr@uvic.ca>
2019-01-09 20:15:09 -0800

    testing goal light

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ token = setup.generate_auth_token(IPaddress)
 myAurora = Aurora(IPaddress, token)
 index = 0
 while index < 4:
-	myAurora.rgb = [255,0,0] #Red
-	sleep(1) #5 seconds seemed like a good time for the red light to stay
-	myAurora.rgb = [255,255,255] #White
-	sleep(0.5)
+	myAurora.rgb = [255,0,0] # Red
+	sleep(2) # Red delay
+	myAurora.rgb = [255,255,255] # White
+	sleep(0.5) # White delay
 	index = index + 1
 myAurora.effect = "Flames"
